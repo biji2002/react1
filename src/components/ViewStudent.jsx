c25cebff6271c18b09cfd4a4f76678308b698c1e
biji2002 <bijivi7777@gmail.com>
2024-06-07 12:29:30 +0530

    completed

---
+++ src/components/ViewStudent.jsx
@@ -0,0 +1,125 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewStudent = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" className="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <p class="card-text">Mariya</p>
+                                        </div>
+                                    
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" className="card-img-top" alt="..."></img>
+                                        <div className="card-body">
+                                            <p className="card-text">Ardhra</p>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" className="card-img-top" alt="..."></img>
+                                        <div className="card-body">
+                                            <p className="card-text">Annitta</p>
+                                        </div>
+                                </div> 
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" className="card-img-top" alt="..."></img>
+                                        <div className="card-body">
+                                            <p className="card-text">Amritha</p>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" className="card-img-top" alt="..."></img>
+                                        <div className="card-body">
+                                            <p className="card-text">Arshitha</p>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" className="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <p class="card-text">Anjitha.</p>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" className="card-img-top" alt="..."></img>
+                                        <div className="card-body">
+                                            <p className="card-text">Anjana</p>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" className="card-img-top" alt="..."></img>
+                                        <div className="card-body">
+                                            <p className="card-text">Arya</p>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" className="card-img-top" alt="..."></img>
+                                        <div className="card-body">
+                                            <p className="card-text">Minnu</p>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" className="card-img-top" alt="..."></img>
+                                        <div className="card-body">
+                                            <p className="card-text">Ann</p>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ViewStudent
